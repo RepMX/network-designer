c5e39e56949665e675758c18d4cd2b8e0d4acc35
RepMX <78993814+RepMX@users.noreply.github.com>
2026-06-04 05:14:41 -0700

    Update eco config

--- eco.config.js
+++ eco.config.js
@@ -4,8 +4,8 @@ module.exports = {
       name: "network-designer",
       script: "./node_modules/next/dist/bin/next",
       args: "start",
-      instances: "2", 
-      exec_mode: "cluster",
+      instances: "1", 
+      exec_mode: "fork",
       cwd: "./",
       env: {
         NODE_ENV: "production",
